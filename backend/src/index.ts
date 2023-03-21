58201667b8540d17e9303c2298f93fb848901124
Albert Gergő <gergoalbert.wod@gmail.com>
2023-03-21 02:36:27 +0200

    feat: set up express

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,2 +1,19 @@
-console.log("hello world");
-console.log("foobar");
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(express.json());
+
+app.get("/", (_, res) => {
+  res.json({
+    message: "hello world",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Running on http://localhost:${PORT}`);
+});
